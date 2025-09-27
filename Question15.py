72bc90cb8bdf900368563978c2612cbf5ded7385
Chirag-2007 <nagpalchirag757@gmail.com>
2025-09-27 11:24:20 +0530

    Question15

---
+++ Question15.py
@@ -0,0 +1,12 @@
+# Even numbers of list
+
+def even_number(num):
+    for i in num:
+        if i % 2 == 0:
+            print(i)
+        else:
+            pass
+    return
+
+number = [1,2,3,4,5,6]
+even_number(number)
