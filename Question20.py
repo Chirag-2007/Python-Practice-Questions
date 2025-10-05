137f178a84871e3215552634cdd8212497906c0c
Chirag-2007 <nagpalchirag757@gmail.com>
2025-10-05 12:15:29 +0530

    Question20

---
+++ Question20.py
@@ -0,0 +1,10 @@
+# Positive/Negative Number
+
+n = int(input("Enter n: "))
+
+if n > 0:
+    print("Positve Number")
+elif n == 0:
+    print("Zero is not positive not negative number")
+else:
+    print("Negative Number")
