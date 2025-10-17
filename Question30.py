cc0ed49403694c42a12b832fb2018969f8cd8136
Chirag-2007 <nagpalchirag757@gmail.com>
2025-10-17 22:21:54 +0530

    Question30

---
+++ Question30.py
@@ -0,0 +1,10 @@
+# Flowing Number Triangle Pattern Printing
+
+n = int(input("Enter n: "))
+num = 1
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(num,end=" ")
+        num = num + 1
+    print()
